94429dd2c93aa3626548ccf7c64b3f2f5d8c1147
Joseph Bethge <Jopyth@users.noreply.github.com>
2018-04-24 13:56:23 +0200

    split and remove images without bounding boxes

--- iywstbdywdiy/sheeper.py
+++ iywstbdywdiy/sheeper.py
@@ -16,7 +16,8 @@ class Generator:
         os.makedirs(self.output, exist_ok=True)
 
         self.i = 0
-        self.info = []
+        self.train_info = []
+        self.test_info = []
 
     def load_stamps(self, stamps):
         self.stamps = []
@@ -38,28 +39,28 @@ class Generator:
             content = json.load(data_handle)
         return content
 
-    def process_image(self, image_path):
+    def process_image(self, image_path, is_test):
         image = Image.open(image_path).convert(mode="RGBA")
 
         bounding_boxes = self.get_data_for(image_path)
 
-        self.make_image(image)
+        # self.make_image(image, is_test)
         for bounding_box in bounding_boxes:
             for stamp in self.stamps:
-                self.make_image(image, [bounding_box], [stamp])
+                self.make_image(image, is_test, [bounding_box], [stamp])
 
-        for nr_bboxes in range(2, min(len(bounding_boxes), 3)):
-            bbox_indices = list(range(len(bounding_boxes)))
-            for i in range(len(bounding_boxes) - nr_bboxes):
-                bbox_indices.remove(random.choice(bbox_indices))
+        for nr_bboxes in range(2, min(len(bounding_boxes), 4)):
+            bboxes = random.sample(bounding_boxes, nr_bboxes)
             stamps = []
-            for i in range(nr_bboxes):
+            for i in range(len(bboxes)):
                 stamps.append(random.choice(self.stamps))
-            self.make_image(image, [bounding_boxes[i] for i in bbox_indices], stamps)
+            self.make_image(image, is_test, bboxes, stamps)
 
-    def make_image(self, image, bounding_boxes=[], stamps=[]):
+    def make_image(self, image, is_test, bounding_boxes=[], stamps=[]):
         output_path = self.get_next_output_path()
-        self.info.append({"image": output_path, "bounding_boxes": bounding_boxes})
+
+        target_info = self.test_info if is_test else self.train_info
+        target_info.append({"image": output_path, "bounding_boxes": bounding_boxes})
 
         out = image
 
@@ -83,16 +84,28 @@ class Generator:
         return os.path.join(self.output, "{:06d}.jpg".format(self.i - 1))
 
     def save_list(self):
-        with open(os.path.join(self.output, "info.json"), "w") as list_file:
-            list_file.write(json.dumps(self.info, indent=2))
+        with open(os.path.join(self.output, "train_info.json"), "w") as list_file:
+            list_file.write(json.dumps(self.train_info, indent=2))
+
+        with open(os.path.join(self.output, "test_info.json"), "w") as list_file:
+            list_file.write(json.dumps(self.test_info, indent=2))
 
 
 def main(args):
-    g = Generator(args.output, args.searchpath)
-    g.load_stamps(args.stamps)
+    prev_state = random.getstate()
+    random.seed(42)
 
-    for image_path in tqdm(args.image):
-        g.process_image(image_path)
+    nr_test_images = int(args.split * len(args.image))
+    is_test = [True] * nr_test_images + [False] * (len(args.image) - nr_test_images)
+    random.shuffle(is_test)
+
+    generator = Generator(args.output, args.searchpath)
+    generator.load_stamps(args.stamps)
+
+    for i, image_path in enumerate(tqdm(args.image)):
+        generator.process_image(image_path, is_test[i])
+
+    random.setstate(prev_state)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Paste a number of images into other images with bounding boxes")
@@ -100,5 +113,6 @@ if __name__ == "__main__":
     parser.add_argument("--searchpath", default=None, help="path to search for corresponding json files")
     parser.add_argument("--stamps", default=["sheep.png"], nargs="+", help="path to search for images to paste")
     parser.add_argument("--output", default="output", help="output directory")
+    parser.add_argument("--split", default=0.2, help="define percentage of images in test data")
 
     main(parser.parse_args())
