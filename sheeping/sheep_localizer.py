3dcf65ae28e899bd60e8588cfd3afd90ef68c679
Christian Bartz <christian.bartz@student.hpi.uni-potsdam.de>
2018-04-27 11:35:21 +0200

    improve fps printing and make font size smaller

--- sheeping/sheep_localizer.py
+++ sheeping/sheep_localizer.py
@@ -21,6 +21,7 @@ class SheepLocalizer:
         self.model = None
         self.mean = self.log.get('image_mean', _imagenet_mean)
         self.font = cv2.FONT_HERSHEY_SIMPLEX
+        self.font_scale = 0.5
         self.color = (0, 255, 0)
         self.initialized = False
 
@@ -78,11 +79,11 @@ class SheepLocalizer:
 
             cv2.rectangle(image, (bbox[1], bbox[0]), (bbox[1] + width, bbox[0] + height), self.color)
 
-            text_size = cv2.getTextSize(str(score), self.font, 1, 1)[0]
+            text_size = cv2.getTextSize(str(score), self.font, self.font_scale, 1)[0]
             text_start = bbox[1] + width - text_size[0], bbox[0] + text_size[1]
             text_end = bbox[1] + width, bbox[0]
             cv2.rectangle(image, text_start, text_end, self.color, -1)
-            cv2.putText(image, str(score), text_start, self.font, 1, (255, 255, 255), bottomLeftOrigin=False)
+            cv2.putText(image, str(score), text_start, self.font, self.font_scale, (255, 255, 255), bottomLeftOrigin=False)
         return image
 
     def localize(self, image):
